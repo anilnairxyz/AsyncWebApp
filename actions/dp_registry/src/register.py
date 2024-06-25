81a544fe7cf3c54ede2c967347e35400d2af4512
Anil Nair <anilnairxyz@gmail.com>
2024-06-25 17:04:03 +0100

    Test github actions

--- actions/dp_registry/src/register.py
+++ actions/dp_registry/src/register.py
@@ -1,8 +1,8 @@
 import os
 
 # get the input and convert it to int
-a = os.environ.get("CLIENT_ID")
-b = os.environ.get("CLIENT_SECRET")
+a = os.getenv("CLIENT_ID")
+b = os.getenv("CLIENT_SECRET")
 num = int(a) + int(b)
 
 # to set output, print to shell in following syntax
