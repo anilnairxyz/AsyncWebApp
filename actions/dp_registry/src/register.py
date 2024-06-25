277947e29caf88f5ec41b071b4a9ad7142917e22
Anil Nair <anilnairxyz@gmail.com>
2024-06-25 16:27:50 +0100

    Test github actions

--- actions/dp_registry/src/register.py
+++ actions/dp_registry/src/register.py
@@ -3,13 +3,7 @@ import os
 # get the input and convert it to int
 a = os.environ.get("CLIENT_ID")
 b = os.environ.get("CLIENT_SECRET")
-if a and b:
-    try:
-        num = int(a) + int(b)
-    except Exception:
-        exit('ERROR: the INPUT_NUM provided ("{}") is not an integer'.format(a))
-else:
-    num = 1
+num = int(a) + int(b)
 
 # to set output, print to shell in following syntax
 print(f"::set-output name=num_squared::{num}")
