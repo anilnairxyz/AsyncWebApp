2b0876f16e2e57a47da793b661b2070bb1b658db
Anil Nair <anilnairxyz@gmail.com>
2024-06-25 21:42:08 +0100

    Test github actions

--- actions/sum_action/calculate_sum.py
+++ actions/sum_action/calculate_sum.py
@@ -11,18 +11,15 @@ def main():
     val_a = int(os.getenv('INPUT_VAL_A'))
     val_b = int(os.getenv('INPUT_VAL_B'))
     numbers_file = os.getenv('INPUT_NUMBERS_FILE')
-    print (numbers_file)
     
     # Read numbers from YAML file
     numbers = yaml.safe_load(numbers_file)
-    print (numbers)
 
-    # yaml_number1 = numbers['numbers']['number1']
-    # yaml_number2 = numbers['numbers']['number2']
+    yaml_number1 = numbers['numbers']['number1']
+    yaml_number2 = numbers['numbers']['number2']
 
     # Calculate the sum
-    # total = val_a + val_b + yaml_number1 + yaml_number2
-    total = val_a + val_b
+    total = val_a + val_b + yaml_number1 + yaml_number2
     
     # Output the result
     print(f"The sum of VAL_A, VAL_B, number1, and number2 is: {total}")
