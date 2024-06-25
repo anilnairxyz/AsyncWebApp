65b3eea801ad85cbee56b28aa8b94286229c7819
Anil Nair <anilnairxyz@gmail.com>
2024-06-25 14:36:41 +0100

    Test github actions

---
+++ actions/dp_registry/src/register.py
@@ -0,0 +1,20 @@
+import os
+import sys
+
+# get the input and convert it to int
+a = os.environ.get("CLIENT_ID")
+b = os.environ.get("CLIENT_SECRET")
+c = sys.argv[1]
+if a and b:
+    try:
+        if c:
+            num = int(a) + int(b) + int(c)
+        else:
+            num = int(a) + int(b)
+    except Exception:
+        exit('ERROR: the INPUT_NUM provided ("{}") is not an integer'.format(a))
+else:
+    num = 1
+
+# to set output, print to shell in following syntax
+print(f"::set-output name=num_squared::{num}")
