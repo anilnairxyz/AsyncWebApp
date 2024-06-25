c1fdb2cc5b77dbb20f1cf72119a3eedc04db4d66
Anil Nair <anilnairxyz@gmail.com>
2024-06-25 23:18:56 +0100

    Test github actions

--- actions/dp_registry/register.py
+++ actions/dp_registry/register.py
@@ -7,10 +7,9 @@ def main():
     a = int(os.getenv('INPUT_CLIENT_ID'))
     b = int(os.getenv('INPUT_CLIENT_SECRET'))
     data_product_spec = os.getenv('INPUT_DP_SPEC')
-    print ("this is ", data_product_spec, a, b)
 
     # Read attributes from data product spec file
-    numbers = json.load(data_product_spec)
+    numbers = json.loads(data_product_spec)
 
     number1 = numbers['numbers']['number1']
     number2 = numbers['numbers']['number2']
